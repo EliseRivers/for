12e2eb16dc4b3afc0186a90d40d5e3333c6af9b8
EliseRivers <the.elizzaveta@gmail.com>
2024-09-04 14:03:08 +0300

    Add files via upload

---
+++ for.py
@@ -0,0 +1,16 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+
+for i in range(len(primes)):
+    print(numbers[i])
+    if i == primes:
+        numbers.remove(i)
+        is_prime True
+        print(numbers)
+
+        for j in range(len(not_primes)):
+            if j == not_primes:
+                numbers.remove(j)
+                not_primes False
+                print(numbers)
